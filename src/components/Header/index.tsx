4c9e96ec4be4db82ad862a6894556ac92eda5323
victorpremoli <victor.premoli57@gmail.com>
2021-03-20 21:23:14 -0300

    challenge completed

--- src/components/Header/index.tsx
+++ src/components/Header/index.tsx
@@ -8,6 +8,7 @@ import { useCart } from "../../hooks/useCart";
 
 const Header = (): JSX.Element => {
   const { cart } = useCart();
+  console.log(cart);
   const cartSize = cart.length; // TODO;
 
   return (
